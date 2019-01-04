7855128bf95a388d3b8a7718eb54996c1be8f28b
Jordi Farras Mañe <jfarras8@hotmail.com>
2019-01-04 13:15:03 +0100

    Ui  and protocols imported

---
+++ faceRecognition/faceRecognition/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  faceRecognition
+//
+//  Created by Jordi Farras Mañe on 04/01/2019.
+//  Copyright © 2019 Jordi Farras Mañe. All rights reserved.
+//
+
+import UIKit
+import CoreImage
+
+class ViewController: UIViewController,UINavigationControllerDelegate,,UIImagePickerControllerDelegate {
+    @IBOutlet weak var myImage: UIImageView!
+    @IBOutlet weak var myTextView: UITextView!
+    
+    @IBAction func `import`(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+   
+    
+}
+
